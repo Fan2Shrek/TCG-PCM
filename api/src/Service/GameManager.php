e6d07ec12086660bdcb4864ddd75aff5d0288633
Pierre Ambroise <pierre27.ambroise@gmail.com>
2026-02-02 17:18:26 +0100

    [API] Make type more precise

--- api/src/Service/GameManager.php
+++ api/src/Service/GameManager.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Room;
+
 final class GameManager
 {
-    public function startGame(object $room): void
+    public function startGame(Room $room): void
     {
         // Implementation of game starting logic goes here.
     }
