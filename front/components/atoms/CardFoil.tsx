505d9b0b3f4ac0a72996f52f06a67a03d7826859
MC <charlie.mass.cm@gmail.com>
2026-02-06 12:54:48 +0100

    ben

--- front/components/atoms/CardFoil.tsx
+++ front/components/atoms/CardFoil.tsx
@@ -12,7 +12,7 @@ const CardFoil = ({ tilt, foil, mask, isHovering, brightness }: CardFoilProps) =
     <div
       className={`
         absolute inset-0 opacity-30
-        bg-glare-effect
+        bg-glare-effect bg-cover
         bg-blend-soft-light-difference
         filter contrast-150 saturate-100 mix-blend-color-dodge
         transition-[filter] duration-300 ease-[cubic-bezier(.2,.9,.2,1)]
@@ -24,7 +24,7 @@ const CardFoil = ({ tilt, foil, mask, isHovering, brightness }: CardFoilProps) =
     />
 
     <div
-      className="absolute inset-0 opacity-30 bg-blend-soft-light-difference"
+      className="absolute inset-0 opacity-30 bg-blend-soft-light-difference bg-cover"
       style={{
         backgroundImage: `url(${mask})`,
       }}
