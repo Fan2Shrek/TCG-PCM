7c6fac335b711f7f870c3224f85bc9ff9650093f
Pierre Ambroise <pierre27.ambroise@gmail.com>
2026-03-06 09:16:36 +0100

    [API] Make character card rare at default

--- api/src/Game/Card/Character/AbstractCharacterCard.php
+++ api/src/Game/Card/Character/AbstractCharacterCard.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Game\Card\Character;
 
+use App\Enum\CardRarityEnum;
 use App\Game\AbstractCard;
 
 abstract class AbstractCharacterCard extends AbstractCard
 {
+    public static CardRarityEnum $rarity = CardRarityEnum::RARE;
+
     abstract public function getHealthPoints(): int;
 }
