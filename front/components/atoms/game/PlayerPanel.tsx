d02f5234fbda98fb75dfe6a52541d2e154347912
Pierre Ambroise <pierre27.ambroise@gmail.com>
2026-03-31 12:41:22 +0200

    [front] Start game board

--- front/components/atoms/game/PlayerPanel.tsx
+++ front/components/atoms/game/PlayerPanel.tsx
@@ -1,19 +1,36 @@
+import Card from "@/components/molecules/Card";
+import PlayerHealthBar from "@/components/molecules/game/PlayerHealthBar";
+import CardsHand from "../../organisms/CardsHand";
+import { GameContext } from "@/context/GameContext";
+import { useContext } from "react";
+
 type Props = {
   player: any;
+  asOpponent?: boolean;
 };
 
-export default ({ player }: Props) => {
+export default ({ player, asOpponent = false }: Props) => {
+  const { getCardById } = useContext(GameContext);
+
   return (
-    <div className="flex items-center gap-6 bg-green-800 p-3 rounded-lg">
-      <div className="font-bold">{player.player.name}</div>
+    <div className={`items-center gap-6 bg-green-800 p-3 rounded-lg`}>
+	  <div className="flex flex-row items-center gap-60">
+		<Card card={getCardById(player.characterCardId)} />
 
-      <div>Coins: {player.coins}</div>
+		{!asOpponent && (
+		  <div className="relative flex gap-2 mt-4 justify-center">
+			<CardsHand cards={player.hand.map((cardId: string) => getCardById(cardId))} />
+		  </div>
+		)}
+	  </div>
 
-      <div>Cards in hand: {player.hand.length}</div>
+	  <div className="mt-2">
+		<PlayerHealthBar health={player.healthPoints} maxHealth={player.maxHealthPoints} />
+	  </div>
 
-      <div>Deck: {player.drawPile.length}</div>
-
-      <div>Discard: {player.discardPile.length}</div>
+	  <div>
+		Draw pile
+	  </div>
     </div>
   );
 }
--- front/components/organisms/game/GameBoard.tsx
+++ front/components/organisms/game/GameBoard.tsx
@@ -197,4 +197,4 @@ export default () => {
       )}
     </div>
   );
-}
+};
