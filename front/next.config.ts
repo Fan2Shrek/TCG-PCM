8bf11b0ae08f93bd22de6bada1b78147967ece04
Pierre Ambroise <74421318+Fan2Shrek@users.noreply.github.com>
2026-04-06 14:41:37 +0200

    [front] Start game board (#114)

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     unoptimized: true, // maybe change
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
