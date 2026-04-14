5bd3d16b0ac8d193920c90da81b60592b64ade7d
Pierre Ambroise <pierre27.ambroise@gmail.com>
2026-04-14 23:27:35 +0200

    [API] Add necromancian card

---
+++ api/src/Game/Card/Character/NecromancianCard.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Game\Card\Character;
+
+use App\Enum\CardRarityEnum;
+use App\Enum\GameEventTypeEnum;
+use App\Game\Card\Interface\TurnAwareInterface;
+use App\Game\Card\Trait\TurnAwareTrait;
+use App\Game\GameContext;
+
+final class NecromancianCard extends AbstractCharacterCard implements TurnAwareInterface
+{
+    public static CardRarityEnum $rarity = CardRarityEnum::EPIC;
+
+    use TurnAwareTrait;
+
+    public function getId(): string
+    {
+        return 'Necromancian';
+    }
+
+    public function getHealthPoints(): int
+    {
+        return 2500;
+    }
+
+    public function onTurnEnd(GameContext $gameContext): void
+    {
+        if (!$this->isOwnerTurn($gameContext)) {
+            return;
+        }
+
+        $owner = $gameContext->state->getPlayer($this->getOwnerId());
+        $pile = $owner->discardPile;
+
+        if (count($pile) > 0) {
+            $index = $gameContext->randomIntBetween(0, count($pile));
+            $cardId = array_keys($pile)[$index];
+
+            $gameContext->pushGameEvent(GameEventTypeEnum::CARD_REDRAWN, [
+                'playerId' => $this->getOwnerId(),
+                'cardId' => $cardId,
+            ]);
+        }
+    }
+}
---
+++ api/tests/Unit/Game/Card/Character/NecromancianCardTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Unit\Game\Card\Character;
+
+use App\Enum\GameEventTypeEnum;
+use App\Game\Card\Character\NecromancianCard;
+use App\Game\GameContext;
+use App\Game\State\GameEvent;
+use App\Tests\Unit\Game\Card\CardTestCase;
+
+final class NecromancianCardTest extends CardTestCase
+{
+    public function getCardFQCN(): string
+    {
+        return NecromancianCard::class;
+    }
+
+    public function testOnTurnEnd()
+    {
+        $card = $this->getCard();
+        $state = $this->createGameContext()->state;
+        $state = $state->withUpdatedPlayer($state->player1->withDiscardedCard('id', 'template'));
+        $ctx = new GameContext($state, '1');
+
+        $card->onTurnEnd($ctx);
+        $events = $ctx->flushEvents();
+
+        self::assertCount(2, $events);
+        self::assertEquals(
+            GameEvent::game(GameEventTypeEnum::CARD_REDRAWN, [
+                'playerId' => '1',
+                'cardId' => 'id',
+            ]),
+            $events[1],
+        );
+    }
+}
