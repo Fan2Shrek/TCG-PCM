44867b9d6ec06961208b41b78ad1252b5ff5b421
Pierre Ambroise <74421318+Fan2Shrek@users.noreply.github.com>
2026-06-03 07:54:15 +0200

    [API][front] Add mercure event on join (#147)

--- front/app/(app)/arene/waiting/[id]/page.tsx
+++ front/app/(app)/arene/waiting/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { use, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import useMercure from '@/hook/useMercure';
 
 import api from "@/lib/api/api";
 
@@ -10,6 +11,7 @@ export default ({ params }: { params: Promise<{ id: string }> }) =>  {
   const router = useRouter()
 
   const [err, setErr] = useState<string|null>(null)
+  const [opp, setOpp] = useState<string|null>(null)
 
   const handleStart = async () => {
 	try {
@@ -21,6 +23,7 @@ export default ({ params }: { params: Promise<{ id: string }> }) =>  {
 	}
   };
 
+
   // Can't use redirect here bc next sucks ass
   const r = (id) => {
 	router.push(`/game/${id}`)
@@ -30,8 +33,19 @@ export default ({ params }: { params: Promise<{ id: string }> }) =>  {
   	navigator.clipboard.writeText(id);
   }
 
+  useMercure(
+    `${process.env.NEXT_PUBLIC_MERCURE_URL}?topic=game/${id}`,
+    {
+	  opponent_joined: (message: { data: { opponent: string }}) => {
+	    setOpp(message.data.opponent);
+	  }
+    },
+  );
+
   return <div className="flex flex-col items-center justify-end h-screen">
 	  {err}
+	  <br />
+	  {opp ? `opponent: ${opp}` : "waiting for opponent..."}
 	  <br />
 	  {id}
 	  <button onClick={handleCopy}>copy id</button>
