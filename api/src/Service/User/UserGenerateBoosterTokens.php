a509972d1204cf8f12b77017f661a144dd81332b
monkyy <charlie.mass.cm@gmail.com>
2026-07-12 21:49:36 +0200

    [back] fixed time til next booster getting reset when user attempted to generate tokens too soon

--- api/src/Service/User/UserGenerateBoosterTokens.php
+++ api/src/Service/User/UserGenerateBoosterTokens.php
@@ -8,7 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 class UserGenerateBoosterTokens
 {
     private const MAX_BOOSTER_TOKENS = 5;
-    private const BOOSTER_TOKEN_INTERVAL_HOURS = 12;
+    private const BOOSTER_TOKEN_INTERVAL_HOURS = 10;
+    private const BOOSTER_TOKEN_INTERVAL_SECONDS = self::BOOSTER_TOKEN_INTERVAL_HOURS * 60 * 60;
 
     public function __construct(
         private EntityManagerInterface $entityManager,
@@ -20,16 +21,16 @@ class UserGenerateBoosterTokens
         $userWallet = $user->getUserWallet();
         $userInfo = $user->getUserInfo();
 
-        $interval = $userInfo->getLastBoosterTokensAt()->diff($now);
-        $hours = (($interval->days ? $interval->days : 0) * 24) + $interval->h;
-        $totalTokens = min(floor($hours / self::BOOSTER_TOKEN_INTERVAL_HOURS) + $userWallet->getBoosterTokens(), self::MAX_BOOSTER_TOKENS);
-        $totalTokens = (int) round($totalTokens, 0);
-        /** @var int $leftoverTime */
-        $leftoverTime = $hours % self::BOOSTER_TOKEN_INTERVAL_HOURS;
+        $elapsedSeconds = max(0, $now->getTimestamp() - $userInfo->getLastBoosterTokensAt()->getTimestamp());
+        $tokensEarned = intdiv($elapsedSeconds, self::BOOSTER_TOKEN_INTERVAL_SECONDS);
+        $totalTokens = min($userWallet->getBoosterTokens() + $tokensEarned, self::MAX_BOOSTER_TOKENS);
 
         $userWallet->setBoosterTokens($totalTokens);
 
-        $userInfo->setLastBoosterTokensAt($now->sub(new \DateInterval('PT'.$leftoverTime.'H')));
+        if ($tokensEarned > 0 || $userWallet->getBoosterTokens() >= self::MAX_BOOSTER_TOKENS) {
+            $leftoverSeconds = $elapsedSeconds % self::BOOSTER_TOKEN_INTERVAL_SECONDS;
+            $userInfo->setLastBoosterTokensAt($now->sub(new \DateInterval('PT'.$leftoverSeconds.'S')));
+        }
 
         $this->entityManager->flush();
 
