1d96d91457cf29aec02e94b9336a0bacb14f5aaa
Fan2Shrek <pierre27.ambroise@gmail.com>
2026-07-13 16:46:10 +0200

    [API] Add maxime and charlie

---
+++ api/src/Game/Card/Character/CharlieCard.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Game\Card\Character;
+
+use App\Game\Card\Interface\TurnAwareInterface;
+use App\Game\Card\Trait\TurnAwareTrait;
+use App\Game\GameContext;
+use Override;
+
+final class CharlieCard extends AbstractCharacterCard implements TurnAwareInterface
+{
+    use TurnAwareTrait;
+
+    public function getId(): string
+    {
+        return 'Charlie';
+    }
+
+    public function getHealthPoints(): int
+    {
+        return 200;
+    }
+
+    public function onTurnStart(GameContext $gameContext): void
+    {
+        if ($gameContext->isCurrentPlayer($this->getOwnerId())) {
+            $gameContext->drawCards(1);
+        }
+    }
+}
