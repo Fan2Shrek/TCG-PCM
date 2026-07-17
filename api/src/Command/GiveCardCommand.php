ccbecc63ba85898a077c74946448c82e4dfcabf8
Pierre Ambroise <74421318+Fan2Shrek@users.noreply.github.com>
2026-07-17 19:42:02 +0200

    [API] Add give command (#215)

---
+++ api/src/Command/GiveCardCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Entity\Inventory\CardInventory;
+use App\Repository\Inventory\CardInventoryRepository;
+use App\Repository\Inventory\InventoryRepository;
+use App\Service\Game\CardRegistryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'app:inventory:add')]
+final class GiveCardCommand
+{
+    public function __construct(
+        private CardRegistryInterface $cardRegistry,
+        private InventoryRepository $inventoryRepository,
+        private EntityManagerInterface $entityManager,
+    ) {}
+
+    public function __invoke(#[Argument('userId')] string $userId, #[Argument('cardId')] ?string $cardId = null, #[Argument('quantity')] int $quantity = 1): int
+    {
+        /** @var \App\Entity\Inventory\Inventory $inv */
+        $inv = $this->inventoryRepository->find($userId);
+
+        if (!$inv) {
+            throw new \RuntimeException(\sprintf('Inventory for user "%s" not found.', $userId));
+        }
+
+        $cards = $inv->getCards();
+
+        foreach ($this->cardRegistry->getAllBy([]) as $card) {
+            $cardInv = $cards->filter(fn(CardInventory $c) => $c->getCard() === $card);
+            $cardInv = $cardInv->isEmpty() ? null : $cardInv->first();
+
+            if (!$cardInv) {
+                $cardInv = new CardInventory($inv, $card);
+                $this->entityManager->persist($cardInv);
+            }
+
+            $cardInv->setQuantity($cardInv->getQuantity() + $quantity);
+        }
+
+        $this->entityManager->flush();
+
+        return 0;
+    }
+}
